92bdae838ba059be31fb10d3791c9836192aa146
murilofelpeto <murilofelpeto@gmail.com>
2020-08-29 21:04:18 -0300

    Created project

---
+++ src/test/java/br/com/murilo/artgalley/ArtgalleyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.murilo.artgalley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtgalleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
